a188600fd833ed4c9e1542107c17ebf589b63161
Kerim Satirli <k.satirli@gmail.com>
2023-02-06 10:20:59 +0100

    linting

--- hcp_vault.tf
+++ hcp_vault.tf
@@ -28,7 +28,7 @@ data "hcp_vault_cluster" "main" {
 # create Vault Admin Token to be able to make changes to HCP Vault configuration
 # ⚠️ Note that this will persist a Vault Admin Token to Terraform State.
 # ⚠️ Disabling this resource does NOT invalidate the provisioned admin token.
-# for more guidance, see https://www.terraform.io/language/state/sensitive-data
+# for more guidance, see https://developer.hashicorp.com/terraform/language/state/sensitive-data
 # and https://registry.terraform.io/providers/hashicorp/hcp/latest/docs/resources/vault_cluster_admin_token
 resource "hcp_vault_cluster_admin_token" "main" {
   cluster_id = data.hcp_vault_cluster.main.cluster_id
