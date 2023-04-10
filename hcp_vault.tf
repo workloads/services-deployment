3bd4ddb57d4d9480ee243655b25bd4d9df48f77c
Kerim Satirli <k.satirli@gmail.com>
2023-04-10 17:08:59 +0200

    bumps HCP Vault module from `1.1.0` to `1.2.0`

--- hcp_vault.tf
+++ hcp_vault.tf
@@ -1,7 +1,7 @@
 module "hcp_vault_aws" {
-  source  = "ksatirli/vault-cluster/hcp"
-  version = "1.1.0"
   # see https://registry.terraform.io/modules/ksatirli/vault-cluster/hcp/latest
+  source  = "ksatirli/vault-cluster/hcp"
+  version = "1.2.0"
 
   cluster_id = "${var.project_identifier}-aws"
   hvn_id     = var.hvn_id_aws
